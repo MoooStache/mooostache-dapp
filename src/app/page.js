bf7a406d53d833952c7258f992ccf7e9f197ce42
Mooo Stache <mooostache.io@gmail.com>
2024-04-27 17:05:29 +0200

    Change Colorschema

--- src/app/page.js
+++ src/app/page.js
@@ -14,24 +14,17 @@ export default function Home() {
       </Head>
 
       {/* Header */}
-      <header className="py-4 px-8 flex items-center justify-between bg-blue-500">
-        <Image
-          src="/MoooStache.jpeg"
-          alt="Mooo Stache - Show your Stache"
-          className="border border-blue-500"
-          width={100}
-          height={100}
-          priority
-        />
-        <h1 className="text-3xl font-bold">Mooo Stache</h1>
+      <header className="px-8 py-4 flex items-center justify-between bg-blue-500">
+        <div className="text-4xl font-light mx-auto">Mooo Stache</div>
       </header>
 
       {/* Navigation */}
-      <nav className="flex justify-center py-4 bg-black">
-        <ul className="flex space-x-6">
-          <li><a href="#" className="text-xl font-medium hover:text-blue-400">Stache</a></li>
-          <li><a href="#" className="text-xl font-medium hover:text-blue-400">Token</a></li>
-          <li><a href="#" className="text-xl font-medium hover:text-blue-400">About Us</a></li>
+      <nav className="flex justify-center py-8 bg-white">
+        <ul className="flex space-x-6 font-light">
+          <li><a href="#" className="text-xl text-blue-500 hover:text-blue-400">Stache</a></li>
+          <li><a href="#" className="text-xl text-blue-500 hover:text-blue-400">Token</a></li>
+          <li><a href="#" className="text-xl text-blue-500 hover:text-blue-400">FAQ</a></li>
+          <li><a href="#" className="text-xl text-blue-500 hover:text-blue-400">About Us</a></li>
         </ul>
       </nav>
 
@@ -39,21 +32,18 @@ export default function Home() {
       <section className="flex-grow flex items-center justify-center">
 
         <div className="flex flex-col text-center justify-center">
-
-
-          <h2 className="text-4xl font-bold mb-4">Welcome to Mooo Stache!</h2>
-          <p className="text-gl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <h2 className="text-4xl font-light mb-4">Welcome to Mooo Stache!</h2>
+          <p className="text-lg font-light">Show your Stache - Coming Q3 2024.</p>
         </div>
       </section>
 
       {/* Footer */}
-      <footer className="py-8 bg-black text-center">
-        <ul className="flex justify-center space-x-6">
-          <li><a href="#" className="text-xl font-medium hover:text-blue-400">Twitter</a></li>
-          <li><a href="#" className="text-xl font-medium hover:text-blue-400">Discord</a></li>
-          <li><a href="#" className="text-xl font-medium hover:text-blue-400">Telegram</a></li>
-          <li><a href="#" className="text-xl font-medium hover:text-blue-400">GitHub</a></li>
-          <li><a href="#" className="text-xl font-medium hover:text-blue-400">FAQ</a></li>
+      <footer className="py-4 bg-white text-center">
+        <ul className="flex justify-center space-x-6 font-light">
+          <li><a href="#" className="text-lg text-blue-500 hover:text-blue-400">Twitter</a></li>
+          <li><a href="#" className="text-lg text-blue-500 hover:text-blue-400">Discord</a></li>
+          <li><a href="#" className="text-lg text-blue-500 hover:text-blue-400">Telegram</a></li>
+
         </ul>
       </footer>
     </div>
